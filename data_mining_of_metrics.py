966842aa7a934daec7a33cfbf9fb29ca2f952d94
Adelaaas <adela.x@yandex.ru>
2020-11-11 22:47:34 +0300

    реализация метода локтя на двух метрках

--- data_mining_of_metrics.py
+++ data_mining_of_metrics.py
@@ -1,10 +1,19 @@
-from prom_to_spark import dataframe_creation
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.evaluation import ClusteringEvaluator
 import matplotlib.pyplot as plt
 from pyspark.ml.feature import VectorAssembler
 import numpy as np
 from pyspark.ml.clustering import BisectingKMeans
+import pyspark as ps
+import findspark
+findspark.init("C:\Spark")
+
+spark = ps.sql.SparkSession.builder \
+    .master("local") \
+    .appName("Dimas Spark") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
 
 #оч полезная статья по kmeans на spark
 #https://rsandstroem.github.io/sparkkmeans.html
@@ -15,7 +24,7 @@ def clustering(dataset):
     #конвертируем данные в float
     # FEATURES_COL = dataset.columns
     # FEATURES_COL.pop(0)
-    FEATURES_COL = ['application_http_status_total','application_httprequests_active']
+    FEATURES_COL = ['_c1','_c10']
     for col in dataset.columns:
         if col in FEATURES_COL:
             dataset = dataset.withColumn(col, dataset[col].cast('float'))
@@ -25,8 +34,8 @@ def clustering(dataset):
 
     # в отличии от sklearn We need to store all features as an array of floats, and store this array as a column called "features"
     vecAssembler = VectorAssembler(inputCols=FEATURES_COL, outputCol="features")
-    df_kmeans = vecAssembler.transform(dataset).select('time', 'features')
-    df_kmeans.show()
+    df_kmeans = vecAssembler.transform(dataset).select('_c0', 'features')
+    # df_kmeans.show()
     #найдем оптимальное k методом локтя
     cost = np.zeros(20)
     for k in range(2, 20):
@@ -34,7 +43,7 @@ def clustering(dataset):
         print('kmeans ', kmeans)
         model = kmeans.fit(df_kmeans.sample(False, 0.1, seed=42))
         cost[k] = model.computeCost(df_kmeans)  # requires Spark 2.0 or later
-    print(cost)
+    # print(cost)
     # вмзуализируем локоть
     fig, ax = plt.subplots(1, 1, figsize=(8, 6))
     ax.plot(range(2, 20), cost[2:20])
@@ -43,16 +52,8 @@ def clustering(dataset):
     plt.show()
 
 
-#создаем датафрейм с метриками
-df = dataframe_creation()
-
+# загружаем метрки из csv
+df = spark.read.csv("metrics.csv")
+df.show()
 # кластеризация данных по метрикам
 clustering(df)
-
-# score = []
-# for i in range(10):
-#     tmp = clustering(df, i)
-#     score.append(tmp)
-#
-# plt.plot(range(1, 8), score, marker='s')
-# plt.show()
